47e22db697b4c51b6f7dfa919181833201fb6c92
doverto1 <40064929+doverto1@users.noreply.github.com>
2025-08-23 20:32:02 -0700

    Remove foosummaries array from Program.cs
    Removed the foosummaries array from Program.cs.

--- api/Program.cs
+++ api/Program.cs
@@ -18,10 +18,6 @@ var newSummaries = new[]
     "Brrrrr", "Yikes", "Frozen", "Hot", "Ouch", "StopIt"
 };
 
-var foosummaries = new[]
-{
-    "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
